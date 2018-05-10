2968a3341c9329a8c8713ea94f9980f2a17dab98
L95e27o <leolovelife6@gmail.com>
2018-05-10 16:22:28 +0800

    Default

--- test2.py
+++ test2.py
@@ -1,22 +1,24 @@
-import random,string
+import random, string
 
-forsSelect = string.ascii_letters+"0123456789"
+forsSelect = string.ascii_letters + "0123456789"
 
-def generate (filename,count,length):
-    f = open(filename,'a')
-    for i in range (count):
-        for m in range (length):
+
+def generate(filename, count, length):
+    f = open(filename, 'a')
+    for i in range(count):
+        for m in range(length):
             f.write(random.choice(forsSelect))
         f.write('\n')
 
     for x in range(count):
         Re = ""
-        for y in range (length):
-            Re+=random.choice(forsSelect)
+        for y in range(length):
+            Re += random.choice(forsSelect)
         print (Re)
+
 
 if __name__ == "__main__":
     filename = 'active_code.txt'
     digit = 20
     num = 200
-    generate(filename,num,digit)
+    generate(filename, num, digit)
